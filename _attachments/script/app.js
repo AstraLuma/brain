e986a6db30848749e61e744498d15ff5de7bdbda
Jamie Bliss <astronouth7303@gmail.com>
2014-01-02 22:11:04 -0500

    hallojs is working

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -3,6 +3,16 @@ $(function() {
     $("#account").couchLogin({
         loggedIn : function(r) {
 			$('#profile').hide();
+			var ops = {
+				plugins: {
+				  'halloformat': {},
+				  'halloblock': {},
+				  'hallojustify': {},
+				  'hallolists': {},
+				  'halloreundo': {}
+				}
+			};
+			$('#title, #body').attr("contenteditable", true).hallo(ops);
         },
         loggedOut : function() {
             $("#profile").html('<p>Please log in to see your profile.</p>').show();
