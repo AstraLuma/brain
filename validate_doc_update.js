668d0ac04cef6164336c5ad0a258191a0aecf7c3
Jamie Bliss <astronouth7303@gmail.com>
2014-01-03 01:19:26 -0500

    Don't automatically give users access

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,25 +1,29 @@
 function (newDoc, oldDoc, userCtx, secObj) {
   var v = require("vendor/couchapp/lib/validate").init(newDoc, oldDoc, userCtx, secObj);
-  
+
   if (v.isAdmin()) {
     return true; // admin can do anything
   }
-  
+
   if (!userCtx.name) {
     // this could be configurable based on secObj
     v.unauthorized("please login to make changes");
   }
-  
+
+  if (userCtx.roles.indexOf(userCtx.db) == -1) {
+    v.unauthorized("request access from admin");
+  }
+
   // only admin may delete
   if (newDoc._deleted) {
     v.unauthorized("only admin may delete docs");
   }
-  
+
   // attached versions must be preserved
   if (oldDoc && oldDoc._attachments) {
     for (var n in oldDoc._attachments) {
       if (n.indexOf("rev") == 0) {
-        if (!(newDoc._attachments && newDoc._attachments[n] 
+        if (!(newDoc._attachments && newDoc._attachments[n]
             && newDoc._attachments[n].stub === true)) {
           v.forbidden("old versions may not be deleted")
         }
