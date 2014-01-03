ae15b0418ebd9ffcf9a6e65fbc74d70a69bfb4db
Jamie Bliss <astronouth7303@gmail.com>
2014-01-03 01:45:24 -0500

    Add newpage dialog

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,13 +1,16 @@
 function save() {
 	var rev = $('article').data('rev');
 	if (rev) {
-		$('article').data('rev', null);
 		var doc = {
 			'_id': document.location.pathname.replace(/^\//, ''),
 			'_rev': rev,
 			'title': $('#title').html(),
 			'body': $('#body').html()
 		};
+		if (!(doc.title && doc.body)) {
+			return;
+		}
+		$('article').data('rev', null);
 		$.ajax({
 			type: "PUT",
 			url: "/!api/"+doc._id,
@@ -49,3 +52,36 @@ $(function() {
         }
     });
  });
+
+$('#create-page').click(function() {
+	$.showDialog('/!script/newpage.html', {
+		submit: function(data, callback) {
+			if (!data.slug || data.slug.length == 0) {
+              callback({slug: "Please enter a slug."});
+              return;
+            }
+            doc = {
+				_id: data.slug,
+				title: data.slug,
+				body: "Page Content Here",
+			};
+			$.ajax({
+				type: "PUT",
+				url: "/!api/"+doc._id,
+				contentType: "application/json",
+				dataType: "json",
+				data: JSON.stringify(doc),
+				complete: function(req) {
+					var resp = $.parseJSON(req.responseText);
+					if (req.status == 200 || req.status == 201 || req.status == 202) {
+						callback();
+						window.location = '/'+doc._id;
+					} else {
+						console.error(resp);
+						callback({slug: resp.reason});
+					}
+				}
+			});
+		}
+	});
+});
